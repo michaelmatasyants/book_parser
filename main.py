a09b65856965b4afcbfda02d6f9397b14698fac1
Michael Matasyants <michael.matasyanc@gmail.com>
2023-08-16 20:13:33 +0300

    Add redirect checker for cases when book is not found

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 import requests
+from requests.exceptions import HTTPError
+
+
+def check_for_redirect(response: requests.models.Response, text):
+    '''Checks are there redirect in history of ressponse'''
+    if response.url == 'https://tululu.org/':
+        raise HTTPError(f'Data for {text} not found')
 
 
 def get_books(number_of_books, path_to_save='books'):
@@ -11,7 +18,11 @@ def get_books(number_of_books, path_to_save='books'):
         book_url = f'https://tululu.org/txt.php?id={book_id}'
         book_response = requests.get(book_url)
         book_response.raise_for_status()
-
+        try:
+            check_for_redirect(book_response, text=f'id{book_id} book')
+        except HTTPError as err:
+            print(err)
+            continue
         with open(Path(path_to_save, f'id{book_id}.txt'), 'wb') as book:
             book.write(book_response.content)
 
