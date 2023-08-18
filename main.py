86577bd03d6b01ff58d616f9bc4ef46909753bff
Michael Matasyants <michael.matasyanc@gmail.com>
2023-08-18 11:23:56 +0300

    Download txt file safely

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 from pathlib import Path
 import requests
 from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filepath, sanitize_filename
 
 
-def check_for_redirect(response: requests.models.Response, text):
+def check_for_redirect(response: requests.models.Response, text=''):
     '''Checks are there redirect in history of ressponse'''
     if response.url == 'https://tululu.org/':
         raise HTTPError(f'Data for {text} not found')
 
 
-def get_books(number_of_books, path_to_save='books'):
-    '''Downloads firs N books (number_of_books) from tululu.org
-       and saves them specified path'''
-
-    Path(path_to_save).mkdir(parents=True, exist_ok=True)
-    for book_id in range(1, number_of_books + 1):
-        book_url = f'https://tululu.org/txt.php?id={book_id}'
-        book_response = requests.get(book_url)
-        book_response.raise_for_status()
-        try:
-            check_for_redirect(book_response, text=f'id{book_id} book')
-        except HTTPError as err:
-            print(err)
-            continue
-        with open(Path(path_to_save, f'id{book_id}.txt'), 'wb') as book:
-            book.write(book_response.content)
+def download_txt(url: str, filename: str, folder='books/') -> Path:
+    """Loads txt file
+       Args:
+        url - link to the text file.
+        filename - The name of the file to save with.
+        folder - Folder to save to.
+       Returns:
+        Path to the file where the text is saved.
+    """
+    sanitized_folder = Path(sanitize_filepath(folder))
+    sanitized_filename = sanitize_filename(filename)
+    sanitized_folder.mkdir(parents=True, exist_ok=True)
+    book_response = requests.get(url)
+    book_response.raise_for_status()
+    full_path = Path(sanitized_folder, f'{sanitized_filename}.txt')
+    try:
+        check_for_redirect(book_response, text='id book')
+    except HTTPError as err:
+        print(err)
+    with open(full_path, 'wb') as book:
+        book.write(book_response.content)
+    return full_path
 
 
 def main():
-    get_books(10)
+    download_txt('http://tululu.org/txt.php?id=1', 'filename')
 
 
 if __name__ == '__main__':
