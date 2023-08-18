b200ac822d4d28aa635af4bfc6aa5f114a14bd88
Michael Matasyants <michael.matasyanc@gmail.com>
2023-08-18 12:42:19 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath, sanitize_filename
 
 
-def check_for_redirect(response: requests.models.Response, text=''):
+def check_for_redirect(url: str, text=''):
     '''Checks are there redirect in history of ressponse'''
+    response = requests.get(url)
+    response.raise_for_status()
     if response.url == 'https://tululu.org/':
-        raise HTTPError(f'Data for {text} not found')
+        raise HTTPError(f'Data for {text} not found.')
 
 
 def get_book_name_and_author(book_url: str) -> tuple:
@@ -35,18 +37,34 @@ def download_txt(url: str, filename: str, folder='books/') -> Path:
     book_response = requests.get(url)
     book_response.raise_for_status()
     full_path = Path(sanitized_folder, f'{sanitized_filename}.txt')
-    try:
-        check_for_redirect(book_response, text='id book')
-    except HTTPError as err:
-        print(err)
     with open(full_path, 'wb') as book:
         book.write(book_response.content)
     return full_path
 
 
+def get_url_of_book_text(book_url: str) -> str:
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    all_tags_a = soup.find(class_='d_book').find_all('a')
+    for tag in all_tags_a:
+        if 'скачать txt' in tag:
+            return f'https://tululu.org/{tag.get("href")}'
+
+
 def main():
-    download_txt('http://tululu.org/txt.php?id=1', 'filename')
-    get_book_name_and_author('https://tululu.org/b1/')
+    for book_id in range(1, 11):
+        book_url = f'https://tululu.org/b{book_id}'
+        try:
+            check_for_redirect(book_url, text=f'book id {book_id}')
+        except HTTPError as err:
+            print(err)
+            continue
+        book_name = f'{book_id} {get_book_name_and_author(book_url)[0]}'
+        book_txt_url = get_url_of_book_text(book_url)
+        if book_txt_url:
+            download_txt(book_txt_url, book_name)
+        else:
+            print(f'Data for "{book_name}" not found.')
 
 
 if __name__ == '__main__':
